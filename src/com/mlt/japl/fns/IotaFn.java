5ff5cd77a9e01a80b6ca19dd9be95572864fe8e7
marko <slarba@kapsi.fi>
2015-07-27 16:25:13 +0300

    dyadic iota

--- src/com/mlt/japl/fns/IotaFn.java
+++ src/com/mlt/japl/fns/IotaFn.java
@@ -1,10 +1,14 @@
 package com.mlt.japl.fns;
 
+import java.util.HashMap;
+
+import com.mlt.japl.arrays.BitArray;
 import com.mlt.japl.arrays.IntArray;
 import com.mlt.japl.arrays.IotaArray;
 import com.mlt.japl.errors.DomainError;
 import com.mlt.japl.errors.ValenceError;
 import com.mlt.japl.iface.Array;
+import com.mlt.japl.tools.Dimensions;
 
 public class IotaFn extends SpecialBaseFn {
 
@@ -16,6 +20,23 @@ public class IotaFn extends SpecialBaseFn {
 		return new IotaArray((int)a.atI(0));
 	}
 
+	@Override
+	public Array dyadic(Array a, Array b, int axis) {
+		HashMap<Array, Integer> map = new HashMap<Array, Integer>();
+		for(int i=0; i<a.length(); i++) {
+			Array key = a.atA(i); 
+			map.putIfAbsent(key, i+1);
+		}
+		
+		int alen = a.length();
+		Array result = new IntArray(new Dimensions(b.length()), new long[b.length()]);
+		
+		for(int i=0; i<b.length(); i++) {
+			result.setI(i, map.getOrDefault(b.atA(i), alen+1));
+		}
+		return result;
+	}
+	
 	@Override
 	public String getName() {
 		return "iota";
