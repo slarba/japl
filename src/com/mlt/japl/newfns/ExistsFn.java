530747520490e2a7a284f4c1b3aa1a798b7010be
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-09-10 09:45:34 +0300

    Fixed existsfn

--- src/com/mlt/japl/newfns/ExistsFn.java
+++ src/com/mlt/japl/newfns/ExistsFn.java
@@ -14,23 +14,23 @@ public class ExistsFn extends BaseFn {
 	@Override
 	public IValue visit_dyadic(IIntArray a, IIntArray b, int axis) {
 		HashSet<Long> set = new HashSet<Long>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
-				return set.contains(b.get(index)) ? 1 : 0;
+				return set.contains(a.get(index)) ? 1 : 0;
 			}			
 		};
 	}
 
 	@Override
 	public IValue visit_dyadic(IIntArray a, IDoubleArray b, int axis) {
-		HashSet<Long> set = new HashSet<Long>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		HashSet<Double> set = new HashSet<Double>();
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
-				return set.contains(b.get(index)) ? 1 : 0;
+				return set.contains(a.get(index)) ? 1 : 0;
 			}			
 		};
 	}
@@ -38,8 +38,8 @@ public class ExistsFn extends BaseFn {
 	@Override
 	public IValue visit_dyadic(IIntArray a, IBitArray b, int axis) {
 		HashSet<Long> set = new HashSet<Long>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
 				return set.contains(b.get(index)) ? 1 : 0;
@@ -49,9 +49,9 @@ public class ExistsFn extends BaseFn {
 
 	@Override
 	public IValue visit_dyadic(IDoubleArray a, IIntArray b, int axis) {
-		HashSet<Double> set = new HashSet<Double>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		HashSet<Long> set = new HashSet<Long>();
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
 				return set.contains(b.get(index)) ? 1 : 0;
@@ -62,20 +62,20 @@ public class ExistsFn extends BaseFn {
 	@Override
 	public IValue visit_dyadic(IDoubleArray a, IDoubleArray b, int axis) {
 		HashSet<Double> set = new HashSet<Double>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
-				return set.contains(b.get(index)) ? 1 : 0;
+				return set.contains(a.get(index)) ? 1 : 0;
 			}			
 		};
 	}
 
 	@Override
 	public IValue visit_dyadic(IDoubleArray a, IBitArray b, int axis) {
-		HashSet<Double> set = new HashSet<Double>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		HashSet<Long> set = new HashSet<Long>();
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
 				return set.contains(b.get(index)) ? 1 : 0;
@@ -86,8 +86,8 @@ public class ExistsFn extends BaseFn {
 	@Override
 	public IValue visit_dyadic(ICharArray a, ICharArray b, int axis) {
 		HashSet<Character> set = new HashSet<Character>();
-		for(int i=0; i<a.length(); i++) set.add(a.get(i));
-		return new LazyBitArray(b.dims()) {
+		for(int i=0; i<a.length(); i++) set.add(b.get(i));
+		return new LazyBitArray(a.dims()) {
 			@Override
 			public long get(int index) {
 				return set.contains(b.get(index)) ? 1 : 0;
--- src/com/mlt/japl/utils/FunctionRegistry.java
+++ src/com/mlt/japl/utils/FunctionRegistry.java
@@ -94,7 +94,7 @@ public class FunctionRegistry {
 		map.put("\u22a3", new StopFn());
 		map.put("\u22a2", new PassFn());
 		map.put("/", new ReplicateFn(false));	
-//		map.put("\u233f", new ReplicateFn(true));
+		map.put("\u233f", new ReplicateFn(true));
 //		map.put("\\", new ExpandFn(false));
 //		map.put("\u2340", new ExpandFn(true));
 //		map.put("\u234b", new GradeUpFn(true));
