9ce92ad1620217904991d35eb8e93d55a47f7e3e
Marko Lauronen <Marko.Lauronen@solita.fi>
2015-09-10 09:10:05 +0300

    replication for other types

--- src/com/mlt/japl/newfns/ReplicateFn.java
+++ src/com/mlt/japl/newfns/ReplicateFn.java
@@ -3,10 +3,18 @@ package com.mlt.japl.newfns;
 import com.mlt.japl.errors.LengthError;
 import com.mlt.japl.errors.RankError;
 import com.mlt.japl.newarrays.IValue;
+import com.mlt.japl.newarrays.concrete.IntArray;
+import com.mlt.japl.newarrays.generated.LazyBitArray;
+import com.mlt.japl.newarrays.generated.LazyCharArray;
+import com.mlt.japl.newarrays.generated.LazyDoubleArray;
 import com.mlt.japl.newarrays.generated.LazyIntArray;
+import com.mlt.japl.newarrays.generated.LazyMixedArray;
 import com.mlt.japl.newarrays.interf.IBitArray;
+import com.mlt.japl.newarrays.interf.ICharArray;
+import com.mlt.japl.newarrays.interf.IDoubleArray;
 import com.mlt.japl.newarrays.interf.IIntArray;
 import com.mlt.japl.newarrays.interf.IIntScalar;
+import com.mlt.japl.newarrays.interf.IMixedArray;
 
 public class ReplicateFn extends BaseFn {
 
@@ -14,6 +22,27 @@ public class ReplicateFn extends BaseFn {
 
 	public ReplicateFn(boolean firstAxis) {
 		this.firstAxis = firstAxis;
+	}
+
+	@Override
+	public IValue visit_dyadic(IBitArray a, IMixedArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		if(a.length()!=b.dims().axis(axis)) throw new LengthError();
+		int n=0, j=0;
+		for(int i=0; i<a.length(); i++) n = n + (int)a.get(i);
+		int[] lookup = new int[n];
+		for(int i=0; i<a.length(); i++) {
+			if(a.get(i)==1) lookup[j++] = i;
+		}
+		return new LazyMixedArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public IValue get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
 	}
 
 	@Override
@@ -32,6 +61,98 @@ public class ReplicateFn extends BaseFn {
 			public long get(int index) {
 				int[] ri = dims().reverseIndexInt(index);
 				ri[axis] = lookup[ri[axis]];
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IBitArray a, ICharArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		if(a.length()!=b.dims().axis(axis)) throw new LengthError();
+		int n=0, j=0;
+		for(int i=0; i<a.length(); i++) n = n + (int)a.get(i);
+		int[] lookup = new int[n];
+		for(int i=0; i<a.length(); i++) {
+			if(a.get(i)==1) lookup[j++] = i;
+		}
+		return new LazyCharArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public char get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IBitArray a, IDoubleArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		if(a.length()!=b.dims().axis(axis)) throw new LengthError();
+		int n=0, j=0;
+		for(int i=0; i<a.length(); i++) n = n + (int)a.get(i);
+		int[] lookup = new int[n];
+		for(int i=0; i<a.length(); i++) {
+			if(a.get(i)==1) lookup[j++] = i;
+		}
+		return new LazyDoubleArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public double get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IBitArray a, IBitArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		if(a.length()!=b.dims().axis(axis)) throw new LengthError();
+		int n=0, j=0;
+		for(int i=0; i<a.length(); i++) n = n + (int)a.get(i);
+		int[] lookup = new int[n];
+		for(int i=0; i<a.length(); i++) {
+			if(a.get(i)==1) lookup[j++] = i;
+		}
+		return new LazyBitArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public long get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IIntArray a, IMixedArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int n=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			n += Math.abs(x);
+		}
+		int[] lookup = new int[n];
+		int j=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			for(int k=0; k<Math.abs(x); k++) {
+				if(x<0) lookup[j++] = -i;
+				else lookup[j++] = i;
+			}
+		}
+		return new LazyMixedArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public IValue get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return IntArray.EMPTY;
 				return b.get(b.dims().calculateIndex(ri));
 			}
 		};
@@ -67,6 +188,93 @@ public class ReplicateFn extends BaseFn {
 	}
 
 	@Override
+	public IValue visit_dyadic(IIntArray a, ICharArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int n=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			n += Math.abs(x);
+		}
+		int[] lookup = new int[n];
+		int j=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			for(int k=0; k<Math.abs(x); k++) {
+				if(x<0) lookup[j++] = -i;
+				else lookup[j++] = i;
+			}
+		}
+		return new LazyCharArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public char get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return ' ';
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IIntArray a, IDoubleArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int n=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			n += Math.abs(x);
+		}
+		int[] lookup = new int[n];
+		int j=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			for(int k=0; k<Math.abs(x); k++) {
+				if(x<0) lookup[j++] = -i;
+				else lookup[j++] = i;
+			}
+		}
+		return new LazyDoubleArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public double get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return 0;
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IIntArray a, IBitArray b, int ax) {
+		if(a.rank()>1) throw new RankError();
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int n=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			n += Math.abs(x);
+		}
+		int[] lookup = new int[n];
+		int j=0;
+		for(int i=0; i<a.length(); i++) {
+			int x = (int)a.get(i);
+			for(int k=0; k<Math.abs(x); k++) {
+				if(x<0) lookup[j++] = -i;
+				else lookup[j++] = i;
+			}
+		}
+		return new LazyBitArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public long get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return 0;
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+	
+	@Override
 	public IValue visit_dyadic(IIntScalar a, IIntArray b, int ax) {
 		int axis = ax<0 ? b.rank()-1 : ax;
 		int x = (int)a.get();
@@ -86,7 +294,70 @@ public class ReplicateFn extends BaseFn {
 			}
 		};
 	}
-	
+
+	@Override
+	public IValue visit_dyadic(IIntScalar a, ICharArray b, int ax) {
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int x = (int)a.get();
+		int n=b.dims().axis(axis) * Math.abs(x);
+		int[] lookup = new int[n];
+		int j=0;
+		for(int k=0; k<b.dims().axis(axis)*Math.abs(x); k++) {
+			lookup[j++] = k/x;
+		}
+		return new LazyCharArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public char get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return ' ';
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IIntScalar a, IDoubleArray b, int ax) {
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int x = (int)a.get();
+		int n=b.dims().axis(axis) * Math.abs(x);
+		int[] lookup = new int[n];
+		int j=0;
+		for(int k=0; k<b.dims().axis(axis)*Math.abs(x); k++) {
+			lookup[j++] = k/x;
+		}
+		return new LazyDoubleArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public double get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return 0;
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
+	@Override
+	public IValue visit_dyadic(IIntScalar a, IMixedArray b, int ax) {
+		int axis = ax<0 ? b.rank()-1 : ax;
+		int x = (int)a.get();
+		int n=b.dims().axis(axis) * Math.abs(x);
+		int[] lookup = new int[n];
+		int j=0;
+		for(int k=0; k<b.dims().axis(axis)*Math.abs(x); k++) {
+			lookup[j++] = k/x;
+		}
+		return new LazyMixedArray(b.dims().replaceAxis(axis, n)) {
+			@Override
+			public IValue get(int index) {
+				int[] ri = dims().reverseIndexInt(index);
+				ri[axis] = lookup[ri[axis]];
+				if(ri[axis]<0) return IntArray.EMPTY;
+				return b.get(b.dims().calculateIndex(ri));
+			}
+		};
+	}
+
 	@Override
 	public String getName() {
 		return "replicate";
